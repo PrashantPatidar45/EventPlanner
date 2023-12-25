f2dfeeaa0d762b690afd94c7898f30318dd555ed
Prashant Patidar <prashantpatidar45@gmial.com>
2023-12-25 21:59:53 +0530

    wedding planner

---
+++ UserService/src/test/java/com/prash/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prash.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
